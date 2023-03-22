77c8685c076d46b105a3230303b3310e6fdda4f8
Idoiasc <128486433+Idoiasc@users.noreply.github.com>
2023-03-22 14:38:48 +0100

    Añadidos comentarios al código

--- programación/Maximum.java
+++ programación/Maximum.java
@@ -11,6 +11,8 @@ public class Maximum {
         return max;
     }
 
+    // Comentario1
+    
     public static void main(String[] args) {
         int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
